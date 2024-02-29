7202ff9af88dd4e34d3eb2fd5da089240a7a3704
sidneychen147 <sidneyyc@uci.edu>
2024-02-28 20:14:48 -0800

    fixed RGB issues

--- firedetector.py
+++ firedetector.py
@@ -16,16 +16,15 @@ class FireDetector(dt.Detector):
         super().__init__()
         self.maxframecount = 2
 
-    def detect(self, image=None):
-        super().detect(image)
+    def detect(self):
         contours = []
 
         # turn current frame into HSV
         hsv = cv2.cvtColor(self.framelist[-1], cv2.COLOR_BGR2HSV)
 
         # Define lower and upper bounds for fire color in HSV
-        lower_bound = np.array([18,50,50])#0, 127, 200])
-        upper_bound = np.array([35, 255, 255])#18, 255, 255])
+        lower_bound = np.array([0, 127, 200])
+        upper_bound = np.array([18, 255, 255])
 
         # Create a binary mask for the fire color range
         fire_mask = cv2.inRange(hsv, lower_bound, upper_bound)
@@ -37,18 +36,17 @@ class FireDetector(dt.Detector):
 
         # Check for fire motion
         if len(self.framelist) >= self.maxframecount:
-            prevframe = cv2.cvtColor(self.framelist[-2], cv2.COLOR_BGR2HSV)[:, :, 2]
-            currframe = hsv[:, :, 2]
-            if (len(prevframe) == len(currframe)):
-                frame_diff = cv2.absdiff(prevframe, currframe)
+            if (len(cv2.cvtColor(self.framelist[-2], cv2.COLOR_BGR2HSV)[:, :, 2]) == len(hsv[:, :, 2])):
+                frame_diff = cv2.absdiff(cv2.cvtColor(self.framelist[-2], cv2.COLOR_BGR2HSV)[:, :, 2], hsv[:, :, 2])
                 _, threshold_diff = cv2.threshold(frame_diff, 10, 255, cv2.THRESH_BINARY)
 
                 # Check for both fire color and fire motion, generate overlapped contours
                 intersection = cv2.bitwise_and(fire_mask, threshold_diff)
                 contours, _ = cv2.findContours(intersection, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # If any regions are detected, identify them
-        for contour in contours:
-            if cv2.contourArea(contour) > 5:
-                x, y, w, h = cv2.boundingRect(contour)
-                cv2.rectangle(self.framelist[-1], (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if len(contours) > 0:
+            # Draw red rectangles around detected regions
+            for contour in contours:
+                if cv2.contourArea(contour) > 5:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(self.framelist[-1], (x, y), (x + w, y + h), (0, 0, 255), 2)
--- webcamtest.py
+++ webcamtest.py
@@ -1,19 +1,19 @@
 import cv2
 from persondetector import findBody, findFace
-from firedetector import FireDetector
+from firedetector import FireDetector, findfire_haar
 
 cap = cv2.VideoCapture(0)
-
 firedetector = FireDetector()
 
 while True:
     _, image = cap.read()
     findBody(image)
     findFace(image)
+
     firedetector.nextframe(image)
     firedetector.detect()
 
-    image = cv2.resize(image, (360, 240))
+    image = cv2.resize(image, (800, 600))
     cv2.imshow("Output", image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
