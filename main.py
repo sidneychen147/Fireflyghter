19b5dc15edad8b270c230fcc96cb9d1d4c420614
sidneychen147 <sidneyyc@uci.edu>
2024-03-13 13:40:27 -0700

    main

--- main.py
+++ main.py
@@ -29,11 +29,10 @@ def process_drone_cam(drone, firedetector):
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         firedetector.nextframe(image)
         fire_detected, coordinates = firedetector.detect()
-        cv2.rectangle(image,(445, 365), (195, 115), (0, 255, 0), 2)
         if temp > 300 and fire_detected:
-            #firedetector.set_text('Real fire detected')
+            firedetector.set_displaytext('Real fire detected')
         if temp < 300:
-            #firedetector.set_text('Potential fire detected')
+            firedetector.set_displaytext('Potential fire detected')
         left_right_velocity = 0
         for_back_velocity = 0
         up_down_velocity = 0
@@ -48,10 +47,10 @@ def process_drone_cam(drone, firedetector):
             errory = objcentery - ycenter
             if abs(errorx) > 125:
                 if errorx < 0:
-                    yaw_velocity = -50
+                    yaw_velocity = -75
                     print("move left")
                 else:
-                    yaw_velocity = 50
+                    yaw_velocity = 75
                     print("move right")
             if abs(errory) > 125:
                 if errory < 0:
@@ -60,16 +59,21 @@ def process_drone_cam(drone, firedetector):
                 else:
                     up_down_velocity = -40
                     print("move down")
-        else:
+        elif temp < 300:
             left_right_velocity = 0
             for_back_velocity = 20
             up_down_velocity = 0
             yaw_velocity = 0
-            print("no")
+        else:
+            left_right_velocity = 0
+            for_back_velocity = 0
+            up_down_velocity = 0
+            yaw_velocity = 0
         image = cv2.resize(image, (640, 480))
         cv2.imshow("Drone camera", image)
-        if start == 0:
+        if cv2.waitKey(1) & 0xFF == ord('e'):
             drone.takeoff()
+            drone.move_up(50)
             start = 1
         if drone.send_rc_control:
             print(f"Battery Level: {drone.get_battery()}")
@@ -80,6 +84,7 @@ def process_drone_cam(drone, firedetector):
             break
 
 # process thermal camera
+
 def process_therm_cam(cap, firedetector):
     global temp
     frame_counter = 0
@@ -93,13 +98,14 @@ def process_therm_cam(cap, firedetector):
             print(f"Battery Level: {drone.get_battery()}")
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+            
 # get max temp
 def get_temp():
     global temp
     while True:
         response = requests.get("http://11.26.15.61:8000/get_temp")
         temp = float(response.text)
+
 
 # Start the threads
 drone_thread = threading.Thread(target=process_drone_cam, args=(drone, firedetector))
